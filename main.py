a854f434e31a793d7f0072baf738e958eee134df
Jooannee <waddermelone0@gmail.com>
2024-10-14 15:35:39 +0200

    Everything works, I think

--- main.py
+++ main.py
@@ -1,33 +1,60 @@
 from browser_history.browsers import Firefox, Chrome, Edge
-import datetime, json, pytz
+import datetime, json, pytz, time
 
 TIMEZONE = pytz.timezone("Europe/Berlin")
-START_TIME = datetime.datetime.now()
-START_TIME = TIMEZONE.localize(START_TIME)
+
+Candidate = input("Enter Candidate ID: \n")
+input ("Press \"Enter\" to Start.")
+
+START_TIME = TIMEZONE.localize(datetime.datetime.now())
+
+print("Confirmed. Start!")
+time.sleep(10)
 
 browserIndex = -1
 while not (browserIndex>0 and browserIndex<4):
-    browserIndex = input("Which Browser will be used? \n 1: Chrome \n 2: Edge \n 3: Firefox \n")
+    browserIndex = input("Which Browser was used? \n 1: Chrome \n 2: Edge \n 3: Firefox \n")
     try:
         browserIndex = int(browserIndex)
     except:
         print("Input is not a number")
         browserIndex = -1
 
-input("Waiting for Confirmation")
+input("Close Browser and press \"Enter\" to end.")
 
 BROWSERS = [Chrome(), Edge(), Firefox()]
 BROWSER = BROWSERS[browserIndex-1]
 fullHistory = BROWSER.fetch_history().histories
 
-print(fullHistory)
 
-# timeDiff = datetime.timedelta(-1)
-# historyIndex = -1
-# while timeDiff.total_seconds()<0:
-#     historyIndex += 1
-#     timeDiff = fullHistory[historyIndex][0] - START_TIME
+historyIndex = -1
+while True:
+    timeDiff = START_TIME - fullHistory[historyIndex][0]
+    historyIndex = historyIndex - 1
+    if timeDiff.total_seconds() >= 0:
+        break
     
+historyIndex = historyIndex + 2
+historyFiltered = fullHistory[historyIndex:]
 
-# historyFiltered = fullHistory[historyIndex:]
-# print(historyFiltered)
+duration = str(TIMEZONE.localize(datetime.datetime.now()) - START_TIME)
+entries = abs(historyIndex)
+
+statistics = {"Duration" : duration,"Entries" : entries}
+historyJSON = []
+
+for i in range(0, entries):
+    historyDict = {"Date" : str(historyFiltered[i][0].date()), "Time" : str(historyFiltered[i][0].time()), "URL" : historyFiltered[i][1], "Title" : historyFiltered[i][2]}
+    historyJSON.append(historyDict)
+
+print(historyJSON)
+with open ("data.json") as file:
+    data = json.load(file)
+
+data["Statistics"]["Total"]["Candidates"] = data["Statistics"]["Total"]["Candidates"] + 1
+data["Statistics"]["Total"]["Entries"] = data["Statistics"]["Total"]["Entries"] + entries
+data["Statistics"][Candidate] = statistics
+data["Histories"][Candidate] = historyJSON
+
+with open ("data.json", 'w') as file:
+    json.dump(data, file, indent=2)
